9557ca57a13e1147579c730a1a2459f2a34136f4
MaxScarp <massy1997.lavoro@gmail.com>
2021-12-29 23:16:01 +0100

    DEVELOPMENT
    Created player class

---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using Aiv.Fast2D;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiCrush
+{
+    class Player
+    {
+
+    }
+}
