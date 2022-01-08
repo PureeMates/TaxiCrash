5c6bea224f28a75f79adbd1d5a266e92854be189
MaxScarp <massy1997.lavoro@gmail.com>
2022-01-08 16:41:53 +0100

    Development
    Created player class, sprite and int movement

--- Player.cs
+++ Player.cs
@@ -10,9 +10,65 @@ namespace TaxiCrush
 {
     class Player
     {
+        private Texture texture;
+        private Sprite sprite;
+
+        private Vector2 velocity;
+        private float speed;
+
+        public bool IsAlive;
+
         public Player()
         {
+            texture = new Texture("Assets/taxi.png");
+            sprite = new Sprite(texture.Width, texture.Height);
+            sprite.pivot = new Vector2(sprite.Width * 0.5f, sprite.Height * 0.5f);
 
+            speed = 700.0f;
+
+            IsAlive = true;
+        }
+
+        public void Input()
+        {
+            if(IsAlive)
+            {
+                MovementInput();
+            }
+        }
+
+        public void Update()
+        {
+            if(IsAlive)
+            {
+                Move();
+            }
+        }
+
+        public void Draw()
+        {
+            sprite.DrawTexture(texture);
+        }
+
+        private void Move()
+        {
+
+        }
+
+        private void MovementInput()
+        {
+            if(Game.Window.GetKey(KeyCode.A) || Game.Window.GetKey(KeyCode.Left))
+            {
+                velocity = new Vector2();
+            }
+            else if(Game.Window.GetKey(KeyCode.D) || Game.Window.GetKey(KeyCode.Right))
+            {
+
+            }
+            else
+            {
+                velocity = new Vector2(0.0f, 0.0f);
+            }
         }
     }
 }
