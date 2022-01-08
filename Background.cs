75e9263347affa4314c4369b3671793e4c8898c7
fede97inno <innofede97@gmail.com>
2022-01-08 16:41:30 +0100

    Development
    Added Class Background

---
+++ Background.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Fast2D;
+using OpenTK;
+
+namespace TaxiCrush
+{
+    class Background
+    {
+        private Sprite sprite;
+        private Sprite sprite2;
+        private Texture texture;
+        private float speed;
+
+        public Background()
+        {
+            texture = new Texture("Assets/BackgroundStreet-1280-720ok.png");
+            sprite = new Sprite(texture.Width, texture.Height);
+            sprite2 = new Sprite(texture.Width, texture.Height);
+            sprite.pivot = new Vector2(0, texture.Height);
+            sprite2.pivot = new Vector2(0, texture.Height);
+            sprite.position = new Vector2(0, Game.Window.Height);
+            sprite2.position = new Vector2(0, -texture.Height);
+            speed = 300.0f;
+        }
+
+        public void Update()
+        {
+            sprite.position.Y += speed * Game.Window.DeltaTime;
+            sprite2.position.Y += speed * Game.Window.DeltaTime;
+            if (sprite.position.Y <= texture.Height)
+            {
+                sprite.position.Y = sprite2.position.Y + texture.Height;
+            }
+            if (sprite2.position.Y <= texture.Height)
+            {
+                sprite2.position.Y = sprite.position.Y + texture.Height;
+            }
+        }
+
+        public void Draw()
+        {
+            sprite.DrawTexture(texture);
+            sprite2.DrawTexture(texture);
+        }
+    }
+}
